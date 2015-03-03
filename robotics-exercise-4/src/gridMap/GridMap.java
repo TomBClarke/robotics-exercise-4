7acba7722c46d883aa3ba03f0af50c7ddd032b92
Thomas Clarke <tomclarke96@hotmail.co.uk>
2015-03-03 16:58:28 +0000

    gridMap interface nearly fully implemented, only 1 method togo

--- robotics-exercise-4/src/gridMap/GridMap.java
+++ robotics-exercise-4/src/gridMap/GridMap.java
@@ -1,49 +1,69 @@
 package gridMap;
 
+import lejos.geom.Line;
 import lejos.geom.Point;
 import rp.robotics.mapping.IGridMap;
+import rp.robotics.mapping.RPLineMap;
 
 public class GridMap implements IGridMap {
+	
+	private final int gridXSize;
+	private final int gridYSize;
+	private final float xStart;
+	private final float yStart;
+	private final float cellSize;
+	private final RPLineMap lineMap;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public GridMap(int gridXSize, int gridYSize, float xStart, float yStart, float cellSize, RPLineMap lineMap){
+		this.gridXSize = gridXSize;
+		this.gridYSize = gridYSize;
+		this.xStart = xStart;
+		this.yStart = yStart;
+		this.cellSize = cellSize;
+		this.lineMap = lineMap;
 	}
 
 	@Override
 	public int getXSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return gridXSize;
 	}
 
 	@Override
 	public int getYSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return gridYSize;
 	}
 
 	@Override
 	public boolean isValidGridPosition(int _x, int _y) {
-		// TODO Auto-generated method stub
-		return false;
+		return _x < gridXSize && _x >= 0 && _y < gridYSize && _y >= 0;
 	}
 
 	@Override
 	public boolean isObstructed(int _x, int _y) {
-		// TODO Auto-generated method stub
-		return false;
+		return !lineMap.inside(getCoordinatesOfGridPosition(_x, _y));
 	}
 
 	@Override
 	public Point getCoordinatesOfGridPosition(int _x, int _y) {
-		// TODO Auto-generated method stub
-		return null;
+		return new Point(xStart + _x * cellSize, yStart + _y * cellSize);
 	}
 
 	@Override
 	public boolean isValidTransition(int _x1, int _y1, int _x2, int _y2) {
-		// TODO Auto-generated method stub
-		return false;
+		if(isObstructed(_x1, _y1) || isObstructed(_x2, _y2)){
+			return false;
+		} else if(Math.sqrt(Math.pow(_x1 - _x2, 2)+Math.pow( _y1 -_y2, 2))!=1) {
+			return false;
+		} else {
+			Line line1 = new Line(_x1, _y1, _x2, _y2);
+			Line[] lines = lineMap.getLines();
+			for (Line line2 : lines) {
+				if(lineMap.intersectsAt(line1, line2) != null){
+					return false;
+				}
+			}
+			return true;
+		}
 	}
 
 	@Override
