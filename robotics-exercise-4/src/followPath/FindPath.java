1c73a8b08710a1c92f9acbd05c429a72b6bc7678
Thomas Clarke <tomclarke96@hotmail.co.uk>
2015-03-17 15:23:16 +0000

    in theory ready for robot, not tested

--- robotics-exercise-4/src/followPath/FindPath.java
+++ robotics-exercise-4/src/followPath/FindPath.java
@@ -36,37 +36,16 @@ public class FindPath {
 		}
 	}
 
-	public IList<Node<Coordinate>> getPath(Coordinate origin,
-			Coordinate coordinate) {
+	public IList<Node<Coordinate>> getPath(Coordinate origin, Coordinate coordinate) {
 
-		AStarFunctions<Node<Coordinate>> functions = new AStarFunctions<Node<Coordinate>>() {
-
+		Predicate<Coordinate> condition = new Predicate<Coordinate>() {
 			@Override
-			public double distanceFunction(Node<Coordinate> node1,
-					Node<Coordinate> node2) {
-				if (node1.successors().contains(node2))
-					return 1.0;
-				else
-					return Double.POSITIVE_INFINITY;
-			}
-
-			@Override
-			public double heuristicFunction(Node<Coordinate> node1,
-					Node<Coordinate> node2) {
-				Coordinate c1 = (Coordinate) node1.contents();
-				Coordinate c2 = (Coordinate) node2.contents();
-
-				return Math.sqrt(Math.pow((c1.x() - c2.x()), 2)
-						+ Math.pow(c1.y() - c2.y(), 2));
+			public boolean holds(Coordinate a) {
+				return a.equals(coordinate);
 			}
 		};
 
-		return graph.findPathFromGeneralisation(
-				graph.nodeWith(origin),
-				graph.nodeWith(coordinate),
-				functions,
-				new MyPriorityQueue<Node<Coordinate>>(functions, graph
-						.nodeWith(new Coordinate(9, 0)))).fromMaybe();
+		return graph.findPathFrom(graph.nodeWith(origin), condition).fromMaybe();
 	}
 
 	public void addObstacle(Coordinate a, Coordinate b) {
